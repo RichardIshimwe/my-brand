b365b51f9f5654e696a50eeeec65f5ffdc0bb115
rich26638 <ishimwerichard26@gmail.com>
2023-02-08 23:25:25 +0200

    validation

--- javascript/general.js
+++ javascript/general.js
@@ -1,106 +1,227 @@
 // window.alert("this is general javascript");
 // let title = document.getElementById("edit_title");
-let terminal = document.getElementById("home_terminal");
-let closed = document.querySelector(".fa-xmark");
+// let terminal = document.getElementById("home_terminal");
+// let closed = document.querySelector(".fa-xmark");
 let command = document.getElementById("command");
 let terminalResult = document.getElementById("terminal_result");
 let responseNav = document.getElementById("responseNav");
-let email_login = document.getElementById("form_input");
-let passcode_login = document.getElementById("login_passcode");
+let emailf_login = document.getElementById("emailf_login");
+let passcode_login = document.getElementById("passcodef_login");
 let emailCheck_login = document.getElementById("emailCheckLogin");
+let emailError_login = document.getElementById("emailLogin_error");
+let passcodeError_login = document.getElementById("errorPasscode_login");
+let checkEmail_login = document.getElementById("checkEmail_login");
+let xpasscode_login = document.getElementById("xpasscode_login");
+var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// ---------------------------------------------------------------------------------------
+let add_title = document.getElementById("add_title");
+let add_textarea = document.getElementById("add_textarea");
+let error_addblog = document.getElementById("errorAdd_title");
+// --------------------------------------------------------------------------------------
+let emailf_signup = document.getElementById("emailf_signup");
+let usernamef_signup = document.getElementById("usernamef_signup");
+let passcodef_signup = document.getElementById("passcodef_signup");
+let confirmf_signup = document.getElementById("confirmf_signup");
+let passcode_signup = document.getElementById("passcodef_signup");
+let emailCheck_signup = document.getElementById("emailCheckLogin");
+let errorEmail_signup = document.getElementById("errorEmail_signup");
+let errorUsername_signup = document.getElementById("errorUsername_signup");
+let errorPasscode_signup = document.getElementById("errorPasscode_signup");
+let errorConfirm_login = document.getElementById("errorConfirm_login");
+let checkEmail_signup = document.getElementById("checkEmail_signup");
+let checkUsername_signup = document.getElementById("checkUsername_signup");
+let checkPasscode_signup = document.getElementById("checkPasscode_signup");
+let checkConfirm_signup = document.getElementById("checkConfirm_signup");
+let signup_array = [];
+let signup_object ={email:"",username:"",passcode:"",confirmp:""};
+let login_object = {email:"",username:""};
+
+let passcodeError_signup = document.getElementById("errorPasscode_signup");
+let xpasscode_signup = document.getElementById("xpasscode_signup");
+
 
 //=========================== login=============================
-function checkLogin(){
-emailCheck_login.style.display = "block";
-    // window.alert("button clicked");
+function checkLogin() {
+
+    let state = 0;
+    if (emailf_login.value == "" && passcode_login.value == "") {
+        emailError_login.innerHTML = "*fields can not be empty";
+        emailf_login.style.border = "2px solid red"
+        passcode_login.style.border = "2px solid red";
+        return false;
+    } else {
+        if (regex.test(emailf_login.value)) {
+            emailf_login.style.border = "2px solid green"
+            checkEmail_login.style.display = "block";
+            emailError_login.innerHTML = "";
+            passcode_login.style.border = "none";
+            state = 10;
+        } else {
+            emailf_login.style.border = "2px solid red"
+            emailError_login.innerHTML = "*please enter the valid email";
+            passcodeError_login.innerHTML = "";
+            passcode_login.style.border = "none";
+            return false;
+        }
+          if (state == 10){
+            if (passcode_login.value == "") {
+                passcodeError_login.innerHTML = "*please enter the password";
+                passcode_login.style.border = "2px solid red";
+            } 
+        }
+    }
+    // console.log("i have reached here");
+login_object = {
+    email: emailf_login.value,
+    passcode: passcode_login.value
+};
+signup_array = JSON.parse(localStorage.getItem('signupFormdata'));
+
+let hold = signup_array.find(object => object.email === login_object.email && object.passcode === login_object.passcode);
+if(hold){
+    // console.log("most welcome to the dashboard");
+    return true;
+}else{
+    console.log("please do a signup");
+    passcode_login.style.border = "none";
+    checkEmail_login.style.display = "none";
+    emailError_login.innerHTML = "";
+    emailf_login.border = "none";
+    passcodeError_login.style.border = "";
+    emailError_login.innerHTML = "*do a signup with the below link";
+}
+
+
+console.log(signup_array);
+
     return false;
 }
 // ===========================end of login=======================
+// +++++++++++++++++++++++++++add blog+++++++++++++++++++++++++++
+function addblog() {
 
-// +++++++++++++++++++++++++++validate email++++++++++++++++++++++++++++
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++signup+++++++++++++++++++++++++++++++++++
-let signup = () => {
-    let email_signup = document.getElementById("signup_email");
-    let username_signup = document.getElementById("signup_username");
-    let pass_signup = document.getElementById("signup_pass");
-    let cpass_signup = document.getElementById("signup_cpass");
-
-    if (email_signup.value == "" && username_signup.value == "" && pass_signup.value == "" && cpass_signup.value == "") {
-        responseNav.innerHTML = "*please fill out the form";
+    if (add_title.value == "" && add_textarea.value == "") {
+        add_title.style.border = "2px solid red"
+        add_textarea.style.border = "2px solid red";
+        error_addblog.innerHTML = "*fields can not be empty";
+    } else {
+        if (add_title.value != "") {
+            add_title.style.border = "2px solid green"
+            error_addblog.innerHTML = "";
+        }
+        else {
+            add_title.style.border = "2px solid red"
+            error_addblog.innerHTML = "";
+        }
+        if (add_textarea.value.length > 5) {
+            add_textarea.style.border = "2px solid green"
+            error_addblog.innerHTML = "";
+        }
+        if (add_textarea.value.length < 5) {
+            add_textarea.style.border = "2px solid red"
+            error_addblog.innerHTML = "*blog description must be greter than 40 character";
+        }
     }
-    // else if (email_signup.value != "") {
-    //     if (email_signup.value == "email") {
-    //         email_signup.style.border = "2px solid green";
-    //     } else {
-    //         responseNav.innerHTML = "*provided email is incorect";
-    //         email_signup.style.border = "2px solid red";
-    //     }
-    // }
-    else if (username_signup.value != "") {
-        if (username_signup.value == "email") {
-            username_signup.style.border = "2px solid green";
-        } else {
-            responseNav.innerHTML = "*please enter the username";
-            username_signup.style.border = "2px solid red";
+    return false;
+}
+// ============================end of blog=======================
+// ==============================signup==========================
+function checkSignup() {
+
+    if (emailf_signup.value == "" && usernamef_signup.value == "" && passcodef_signup.value == "" && confirmf_signup.value == "") {
+        emailf_signup.style.border = "2px solid red"
+        usernamef_signup.style.border = "2px solid red";
+        passcodef_signup.style.border = "2px solid red"
+        confirmf_signup.style.border = "2px solid red";
+        errorEmail_signup.innerHTML = "*please fill out the form";
+        return false;
+    } else {
+        if (emailf_signup.value != "" && regex.test(emailf_signup.value)) {
+            emailf_signup.style.border = "2px solid green"
+            checkEmail_signup.style.display = "flex";
+            errorEmail_signup.innerHTML = "";
+        }
+        else {
+            emailf_signup.style.border = "2px solid red"
+            errorEmail_signup.innerHTML = "*please enter the valid email";
+            return false;
+        }
+        if (usernamef_signup.value != "") {
+            usernamef_signup.style.border = "2px solid green"
+            checkUsername_signup.style.display = "flex";
+            errorUsername_signup.innerHTML = "";
+        }
+        else {
+            usernamef_signup.style.border = "2px solid red"
+            errorUsername_signup.innerHTML = "*please enter the valid username";
+            return false;
+        }
+        if (passcodef_signup.value.length > 4) {
+            passcodef_signup.style.border = "2px solid green";
+            checkPasscode_signup.style.display = "flex";
+            passcodeError_signup.innerHTML = "";
+        }
+        else {
+            passcodef_signup.style.border = "2px solid red"
+            passcodeError_signup.innerHTML = "*passcode must be greater then 8 characters";
+            return false;
+        }
+        if (confirmf_signup.value != "") {
+            if (confirmf_signup.value === passcodef_signup.value) {
+                confirmf_signup.style.border = "2px solid green";
+                checkConfirm_signup.style.display = "flex";
+                errorConfirm_login.innerHTML = "";
+            }
+            else {
+                confirmf_signup.style.border = "2px solid red"
+                errorConfirm_login.innerHTML = "*password does not match";
+                return false;
+            }
+        }
+        else {
+            confirmf_signup.style.border = "2px solid red"
+            errorConfirm_login.innerHTML = "*please confirm the password";
+            return false;
         }
     }
 
-
-    //    if(email_signup.value == "" && username_signup.value == "" && pass_signup.value=="" && cpass_signup.value == ""){
-    //         responseNav.innerHTML = "*please fill out the form";
-    //         // email_signup.style.border = "2px solid red";
-    //         // username_signup.style.border = "2px solid red";
-    //         // pass_signup.style.border = "2px solid red";
-    //         // cpass_signup.style.border = "2px solid red";
-    //    }
-    //    else if(email_signup.value != ""){
-    //    if(validateEmail(email_signup.value) == false){
-    //     email_signup.style.border = "2px solid green";
-    //    }else{
-    //     email_signup.style.border = "2px solid red";
-    //    }
-    // }
+signup_object ={email: emailf_signup.value, 
+                username: usernamef_signup.value,
+                passcode: passcodef_signup.value,
+                confirmp: confirmf_signup.value};
+signup_array = JSON.parse(localStorage.getItem('signupFormdata'));
+let hold = signup_array.find(object => object.username === signup_object.username);
+if(hold){
+    errorEmail_signup.style.color = "green";
+    errorEmail_signup.innerHTML = "You already have an account";
+}else{
+signup_array.push(signup_object);
+localStorage.setItem('signupFormdata',JSON.stringify(signup_array));
+console.log(JSON.parse(localStorage.getItem('signupFormdata')));
+emailf_signup.value = ""; 
+usernamef_signup.value = "";
+passcodef_signup.value = "";
+confirmf_signup.value = "";
 }
+return true;
+}
+// ============================end of signup=====================
 // ++++++++++++++++++++++++++login functions++++++++++++++++++++++++++++
-let login = () => {
-    if (email_login.value != "") {
-        window.location.href = './html/dashboard.html';
-    }else {
-        window.alert("please enter something")
-    }
 
-}
-
-// ======================================================social medias=====================================
-function linkedin() {
-    window.open('https://www.linkedin.com/in/ishimwe-richard-40175a24a/', '_blank');
-    // window.alert("linledin is clicked");
-}
-function github() {
-    window.open('https://github.com/rich26638', '_blank');
-}
-// ===================================================end of socialmedia===================================
 closed.addEventListener('click', () => {
     document.querySelector(".terminal").style.display = "none";
     document.querySelector(".home_profile").style.display = "flex";
 })
-
-
 terminal.addEventListener('click', () => {
 
     document.querySelector(".terminal").style.display = "block";
     document.querySelector(".home_profile").style.display = "none";
 
 })
-
-
 function edit() {
     let edit = document.getElementById("edit_click");
     window.location.href = 'editblog.html';
 }
-
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter' && command.value != "") {
         if (command.value === "login") {
