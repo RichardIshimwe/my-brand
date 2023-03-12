150d432c5ac9adf9748d647d031acfabf39f2e00
rich26638 <ishimwerichard26@gmail.com>
2023-03-12 11:50:45 +0200

    adding and editing blog with the image from cloudinary

--- javascript/addeditblog.js
+++ javascript/addeditblog.js
@@ -42,36 +42,75 @@ add_image.addEventListener('change', function () {
     })
 });
 
-    addBlog.addEventListener('submit', (event) =>{
+
+
+addBlog.addEventListener('submit', (event) =>{
     event.preventDefault();
     if(add_title.value != "" && add_textarea.value != ""){
     let actualDate = `${day}-${months[month]}-${year}`;
-    // const file = fileInput.files[0];
-    const file = localStorage.getItem('image')
-    let blogsTosend = {
-        token: token,
-        image: file,
-        title: add_title.value,
-        description: add_textarea.value,
-        };
-        console.log(blogsTosend)
+    let fileToget = document.getElementById('add_image').files[0];
+    let title = document.getElementById('add_title').value;
+    let description = document.getElementById('add_textarea').value;
+    const formData = new FormData();
+    formData.append('image', fileToget);
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('author',logedIn)
+    console.log(title,fileToget)
         fetch('https://puce-helpful-xerus.cyclic.app/blogs', {
             method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(blogsTosend)
+            body: formData,
+            mode: 'no-cors'
           })
-          .then(response => response.json())
-          .then(resp => {
+          .then(response => {
+            console.log("image uploaded")
             location.href = 'https://my-brand-richard.netlify.app/html/manage.html'
         })
+          .then(resp => {
+        }).catch(err => console.log(err))
+
 }
     else{
         add_title.style.border = "2px solid red" 
         add_textarea.style.border = "2px solid red"
     }
 })
+
+
+
+
+
+
+//     addBlog.addEventListener('submit', (event) =>{
+//     event.preventDefault();
+//     if(add_title.value != "" && add_textarea.value != ""){
+//     let actualDate = `${day}-${months[month]}-${year}`;
+//     // const file = fileInput.files[0];
+//     const file = localStorage.getItem('image')
+//     let blogsTosend = {
+//         token: token,
+//         image: file,
+//         title: add_title.value,
+//         description: add_textarea.value,
+//         };
+//         console.log(blogsTosend)
+//         fetch('https://puce-helpful-xerus.cyclic.app/blogs', {
+//             method: 'POST',
+//             headers: {
+//               'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(blogsTosend)
+//           })
+//           .then(response => response.json())
+//           .then(resp => {
+//             location.href = 'https://my-brand-richard.netlify.app/html/manage.html'
+//         })
+// }
+//     else{
+//         add_title.style.border = "2px solid red" 
+//         add_textarea.style.border = "2px solid red"
+//     }
+// })
 
 function edit() {
     window.location.href = 'https://my-brand-richard.netlify.app/html/editblog.html';
